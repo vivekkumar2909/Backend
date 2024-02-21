c0c045aac56e294245717937a40dd6aad5ddcf73
vivekkumar2909 <rajvivek620065@gmail.com>
2024-02-21 10:21:49 +0530

    First Commit of Backend

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT ;
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.get('/login', (req, res) => {
+    res.send('<h1>Login Page</h1>');
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}!`);
+});
