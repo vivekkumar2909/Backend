7c572e3a6b91d09b3e0a79713740ce14caac20fb
vivekkumar2909 <rajvivek620065@gmail.com>
2024-02-29 09:39:10 +0530

    "GitIgnore for Ecommerce"

--- Ecommerce/models/ecommerce/user.model.js
+++ Ecommerce/models/ecommerce/user.model.js
@@ -1,7 +1,20 @@
 import mongoose from 'mongoose';    
 
-const userSchema = new mongoose.Schema({},{timestamps: true}){
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+},{timestamps: true})
 
-}
+
 
 export default mongoose.model('User', userSchema);
